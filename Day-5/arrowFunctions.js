02e2d0250cfa4b18e74068f54b19bc99afeb078e
jkansra.tech@gmail.com <jai.kansra@kuliza.com>
2019-04-24 20:33:51 +0530

    JS Day 5 - Arrow Functions, Inheritance, Template Literals

---
+++ Day-5/arrowFunctions.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/*
+ * Modify and return the array so that all even elements are doubled and all odd elements are tripled.
+ * 
+ * Parameter(s):
+ * nums: An array of numbers.
+ */
+function modifyArray(nums) {
+    var arr = nums.map(num => num % 2 === 0 ? num * 2 : num * 3);
+    return arr;
+}
---
+++ Day-5/templateLiterals.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/*
+ * Determine the original side lengths and return an array:
+ * - The first element is the length of the shorter side
+ * - The second element is the length of the longer side
+ * 
+ * Parameter(s):
+ * literals: The tagged template literal's array of strings.
+ * expressions: The tagged template literal's array of expression values (i.e., [area, perimeter]).
+ */
+function sides(literals, ...expressions) {
+    var arr = [];
+    let area = expressions[0];
+    let perimeter = expressions[1];
+    let s1 = (perimeter + Math.sqrt(((perimeter * perimeter) - (16 * area)))) / 4;
+    let s2 = (perimeter - Math.sqrt(((perimeter * perimeter) - (16 * area)))) / 4;
+    arr.push(s1);
+    arr.push(s2);
+    arr = arr.sort(function (x, y) {
+        return x > y;
+    });
+    return arr;
+}
